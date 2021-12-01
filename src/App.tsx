73ba5706c7b32a9568871d4557d9f42fd06fdb37
xuchaoyang <1034705317@qq.com>
2021-12-01 21:31:01 +0800

    增加注册和登录的界面

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-// import {ProjectListScreen} from './screens/project-list'
-import { LoginScreen } from "./screens/login";
+import { AuthenticatedApp } from "./authenticated-app";
+import { useAuth } from "./context/auth-context";
+import { UnauthenticatedApp } from "./unauthenticated-app";
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      {/* <ProjectListScreen></ProjectListScreen> */}
-      <LoginScreen></LoginScreen>
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
